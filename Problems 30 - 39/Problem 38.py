2f4d42f33de3188a775c75c93f30a73bd476f2d5
aryanmalik567 <aryanmalik567@gmail.com>
2020-07-09 14:09:53 +0100

    Solved problem 38

--- Problems 30 - 39/Problem 38.py
+++ Problems 30 - 39/Problem 38.py
@@ -1,14 +1,16 @@
-def consecutiveNum(number):
-    output = []
-    for i in range(1, number + 1):
-        output.append(i)
-    return output
+nineList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+pandigitals = [1]
+end = False
 
 
-n = 1
-integer = 1
-pandigital = 1
-while len(str(pandigital)) < 10:
-    multipliers = []
-    for multiplier in range(1, n):
-        multipliers.append(multiplier)
+for integer in range(9123, 9876):
+
+    product = 100000 * integer + 2 * integer
+    productDigits = []
+    for i in str(product):
+        productDigits.append(int(i))
+    if sorted(productDigits) == nineList:
+        pandigitals.append(product)
+
+print(pandigitals)
